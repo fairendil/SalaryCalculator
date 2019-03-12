645aa2cbce3d63815e75ea13101b9db774b58090
fairendil <fairendil@gmail.com>
2019-03-12 10:29:08 +0300

    Created basic project structure

---
+++ Base/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Models
+{
+    public class Employee
+    {
+
+        public int EmployeeId { get; set; }
+
+        public DateTime EmploymentDate { get; set; }
+
+        public decimal BaseSalary { get; set; }
+
+    }
+}
